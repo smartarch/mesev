f81dc9d6a1daa52c20ec96f1b8a55a43aa23969c
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2025-06-20 10:01:13 +0000

    Fix typo

--- editor/dsl/launch.sh
+++ editor/dsl/launch.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-JAR_FILE="/opt/com.mseve.dsl.parent/com.mseve.dsl.ide/target/com.mesev.dsl.ide-1.0.0-SNAPSHOT-ls.jar"
+JAR_FILE="/opt/com.mesev.dsl.parent/com.mesev.dsl.ide/target/com.mesev.dsl.ide-1.0.0-SNAPSHOT-ls.jar"
 
 while [ -f "$JAR_FILE" ] ; do
-        java -jar "$JAR_FILE"
+        java -jar "$JAR_FILE" 2>/opt/logs/lang_server_error.log >/opt/logs/lang_server_access.log
 done
 
 echo "cannot find $JAR_FILE"
