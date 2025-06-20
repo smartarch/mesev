21c1bc5abaef12b5d79844ea5736d41835af127d
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2025-06-20 13:03:43 +0200

    Add vscode ide

---
+++ editor/vscode/run.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+CODE_SERVER_HOST="${CODE_SERVER_HOST:-localhost}"
+CODE_SERVER_PORT="${CODE_SERVER_PORT:-8080}"
+CODE_SERVER_ORIGIN="${CODE_SERVER_ORIGIN:-localhost}"
+CODE_SERVER_WELCOME_TEXT="${CODE_SERVER_WELCOME_TEXT:-welcome}"
+CODE_SERVER_APP_NAME="${CODE_SERVER_APP_NAME:-MesevDSL}"
+
+code-server /home/user/workspace --auth none --bind-addr "$CODE_SERVER_HOST":$CODE_SERVER_PORT --app-name=$CODE_SERVER_APP_NAME --welcome-text=$CODE_SERVER_WELCOME_TEXT --disable-update-check --disable-workspace-trust --trusted-origins $CODE_SERVER_ORIGIN 2>/opt/logs/ide_error.log >/opt/logs/ide_access.log
