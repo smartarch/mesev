904196e556e6cd0abfd43ac285977f3b4943100d
Milad Abdullah <abdullah@d3s.mff.cuni.cz>
2025-07-02 17:42:53 +0200

    Add portal

---
+++ portal/web-app/src/containers/Dashboard/DDM.tsx
@@ -0,0 +1,17 @@
+import './common.scss';
+import './experiments.scss';
+
+const DDM = () => {
+
+  return (
+    <>
+      <div className="page dashboard__page">
+        <iframe width="100%" height="100%" style={{border:'none'}}
+            src="http://146.124.106.200"
+        ></iframe>
+      </div>
+    </>
+  );
+};
+
+export default DDM;
