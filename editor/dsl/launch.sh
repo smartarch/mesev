9e2936337d5ebcbb0fd62e3cd7b308ab431459de
abdullah <abdullah@d3s.mff.cuni.cz>
2025-07-06 18:53:20 +0000

    Modify example

--- editor/dsl/launch.sh
+++ editor/dsl/launch.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-JAR_FILE="/opt/com.mesev.dsl.parent/com.mesev.dsl.ide/target/com.mesev.dsl.ide-1.0.0-SNAPSHOT-ls.jar"
+JAR_FILE="/opt/cz.smartarch.yamas.dsl.parent/cz.smartarch.yamas.dsl.ide/target/cz.smartarch.yamas.dsl.ide-1.0.0-SNAPSHOT-ls.jar"
 
 while [ -f "$JAR_FILE" ] ; do
         java -jar "$JAR_FILE" 2>/opt/logs/lang_server_error.log >/opt/logs/lang_server_access.log
